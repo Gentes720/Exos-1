ec539cc43cdf1847bd9cb428f3dcfa64982b1b28
Gentes720 <lontsiangelin@gmail.com>
2024-11-28 22:03:02 +0100

    correction 18

--- 018.py
+++ 018.py
@@ -1,18 +1,32 @@
-def prime(number):
-    i , c = 1 , 0
-    while i <= number/2 :
-        if number % i == 0:
-            c += 1 # c changes when there is at least one number different from 1 and number which is a divider of number
-        i += 1
-    if c == 1:
-        return True
-    else : 
-        return False
 
-CountNumPrime = 0
-while CountNumPrime < 100 :
-    for i in range(1, 560, 1):
-        if prime(i):
-            print(i)
-            CountNumPrime += 1
-    
+import numpy as np
+
+def prime(n):
+    if n < 1:
+        return False
+    if n == 1:
+        return True
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(np.sqrt(n)) + 1
+    for i in range(3, max_divisor, 2):
+        if n % i == 0:
+            return False
+    return True
+
+# creation of an array of 100 element
+array = np.zeros(100, dtype=int)
+
+# Research of the 100 first prime numbers
+count = 0
+num = 1
+while count < 100:
+    if prime(num):
+        array[count] = num
+        count += 1
+    num += 1
+
+# results
+print(array)
