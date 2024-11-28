a4daa7c84652f00fb1f1fa175a2cd781895fabeb
Gentes720 <lontsiangelin@gmail.com>
2024-11-28 22:04:45 +0100

    correction 22

--- 024.py
+++ 024.py
@@ -1,20 +1,5 @@
-array1, array2 = [], []
-lenth1 = int(input("enter lenth of array1 "))
-i=0
-while i < lenth1 :
-    print("Enter the value of the ",i+1," value" )
-    value = input()
-    array1.insert(i, value)
-    i += 1
-print(array1)
-lenth2 = int(input("enter lenth of array2 "))
-i=0
-while i < lenth2 :
-    print("Enter the value of the ",i+1," value" )
-    value = input()
-    array2.insert(i, value)
-    i += 1
-print(array2)
-
-array1.extend(array2)
-print(array1)
+import numpy as np
+array1, array2 = np.arange(6) , [6, 7, 8, 9, 10, 11] 
+stackarray = np.hstack((array1, array2))
+# hstack merge arrays
+print(stackarray)
