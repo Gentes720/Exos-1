6e4da66f7f1f9b1b55fa56c740c7c3f269662376
Gentes720 <lontsiangelin@gmail.com>
2024-11-28 22:04:24 +0100

    correction 22

--- 022.py
+++ 022.py
@@ -1,15 +1,10 @@
-array = []
-lenth = int(input("enter lenth of array "))
+import numpy as np
+
+array = np.arange(10).reshape(1,10)
 i=0
-while i < lenth :
-    print("Enter the value of the ",i+1," value" )
-    value = input()
-    array.insert(i, value)
-    i += 1
-print(array)
-i=0
-reverse = [] # exit array
-while i < lenth :
-    reverse.insert(i,array[lenth -1- i]) # inverse filling, last value in array as first in reverse
+reverse = np.empty( (1, 10) ) # exit array
+while i < 10 :
+    reverse[0, i] = array[0, 10 -1- i] 
+    # inverse filling, last value in array as first in reverse
     i+=1
 print(reverse)
